bd95fae0bb965882888acc9d73a3bd52ef7b1670
Matthew Glum <mtglum@gmail.com>
2020-05-22 16:35:07 -0700

    Change attribution links to target=_blank

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var map = L.map('map');
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
 }).addTo(map);
 
 fetch('https://geo.weather.gc.ca/geomet/?lang=en&service=WFS&REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&TYPENAME=ec-msc:CURRENT_CONDITIONS&outputFormat=GEOJSON&srsName=EPSG%3A3857', {
@@ -32,7 +32,7 @@ fetch('https://geo.weather.gc.ca/geomet/?lang=en&service=WFS&REQUEST=GetFeature&
     pointToLayer: function (feature, latlng) {
       return createMarker(feature, latlng);
     },
-    attribution: 'Data Source: <a href="https://www.canada.ca/en/environment-climate-change/services/weather-general-tools-resources/weather-tools-specialized-data/geospatial-web-services.html">Environment and Climate Change Canada</a>'
+    attribution: 'Data Source: <a href="https://www.canada.ca/en/environment-climate-change/services/weather-general-tools-resources/weather-tools-specialized-data/geospatial-web-services.html" target="_blank">Environment and Climate Change Canada</a>'
   }).addTo(map);
 
   map.fitBounds(layer.getBounds());
